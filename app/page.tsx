515837835b401ae5f25e1fda53b132eb9cf63b31
dgcosta671-a11y <dgcosta.671@gmail.com>
2026-01-08 12:03:02 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>App funcionando 🚀</h1>
-      <p>Projeto recriado do zero com sucesso.</p>
-    </main>
-  );
-}
